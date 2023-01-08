0838ec2c083761528be216505481917e0f701a74
VladMit1 <mitin1508@gmail.com>
2023-01-09 00:02:58 +0100

    fix4

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -81,7 +81,7 @@ router.post('/login', async (req, res) => {
 
 router.get('/auth', authMiddleware, async (req, res) => {
    try {
-      const user = await User.findOne({ id: req.user });
+      const user = await User.findOne({ id: req.user.id });
       const token = jwt.sign({ id: user.id }, config.get('secretKey'), {
          expiresIn: '1h'
       });
